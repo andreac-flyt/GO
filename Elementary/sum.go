580e689195296409ee10f5d102ab4d9a06683e8a
andreac <andrea.czarnecki@flyt.io>
2021-04-30 19:40:58 -0400

    adding new formulae

--- Elementary/sum.go
+++ Elementary/sum.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 
 	addNums()
-	addAll()
+	addMultiples()
 }
 
 func addNums() {
@@ -32,4 +32,18 @@ func addAll() {
 		total += i
 	}
 	fmt.Printf("The total sum of all numbers from 1 to %v is %v.\n", n, total)
+}
+
+func addMultiples() {
+
+	var num int
+
+	fmt.Print("Please enter a whole number: ")
+	fmt.Scanf("%v", &num)
+	var total int
+	for n := 3; n < num && n%3 == 0; n++ {
+		total += n
+	}
+	fmt.Printf("The total sum of all numbers divisible by 3 to %v is equal to %v.\n", num, total)
+
 }
